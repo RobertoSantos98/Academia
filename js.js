299bea7e0063ae949dba6f90d00a98b5755927c9
RobertoSantos98 <146374934+RobertoSantos98@users.noreply.github.com>
2023-10-13 19:21:21 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+const formulario = document.getElementById("formulario");
+const nome = document.getElementById("nome");
+const sobrenome = document.getElementById("Sobrenome");
+const email = document.getElementById("email");
