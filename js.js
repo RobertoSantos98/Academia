91905aaf0827af137f03e66159502ed7142cbdf3
RobertoSantos98 <146374934+RobertoSantos98@users.noreply.github.com>
2023-10-27 08:56:20 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,19 @@
-const formulario = document.getElementById("formulario");
-const nome = document.getElementById("nome");
-const sobrenome = document.getElementById("Sobrenome");
-const email = document.getElementById("email");
+const form = document.getElementById('formulario');
+const campo = document.getElementById('nome');
+const span = document.querySelectorAll('.span-required');
+
+function setError(index){
+    campo[index].style.border = '2px solid #e63636';
+    span[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campo.value.lenght < 4){
+        console.log('Nome deve ter 3 caracteres');
+    } 
+    else{
+        console.log('Nome validado');
+    }
+}
+
+
